a159d3ebdf3c04b17a18f7c310ac121027534cca
chitinglow <ctlow83@gmail.com>
2023-05-28 21:00:43 +0800

    new test

--- test.py
+++ test.py
@@ -109,3 +109,68 @@ with resultCol2:
             f'<div class="result">Tails percentage: {tails_percentage} %</div>',
             unsafe_allow_html=True
         )
+
+
+def count_flips(n):
+    heads = 0
+    tails = 0
+
+    for _ in range(n):
+        flip = random.randint(0, 1)
+        if flip == 1:
+            heads += 1
+        else:
+            tails += 1
+
+    heads_percentage = 100.0 * heads / n
+    tails_percentage = 100.0 * tails / n
+
+    return heads, tails, heads_percentage, tails_percentage
+
+code2 = """
+        def count_flips(n):
+            heads = 0
+            tails = 0
+
+            for _ in range(n):
+                flip = random.randint(0, 1)
+                if flip == 1:
+                    heads += 1
+                else:
+                    tails += 1
+
+            heads_percentage = 100.0 * heads / n
+            tails_percentage = 100.0 * tails / n
+
+            return heads, tails, heads_percentage, tails_percentage
+
+        n = 1000000
+        heads_count, tails_count, heads_percentage, tails_percentage = count_flips(n)
+"""
+codeCol2, resultCol2 = st.columns([2,1])
+with codeCol2:
+    st.code(code2, language='Python')
+
+
+with resultCol2:
+    if st.button("Run",key="btn3"):
+        n = 1000000
+        heads_count, tails_count, heads_percentage, tails_percentage = count_flips(n)
+        bg_color = "#ccffcc"
+        st.markdown(
+            f"""
+            <style>
+            .result {{
+                background-color: {bg_color};
+            }}
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+        st.markdown(
+            f'<div class="result">Heads count: {heads_count}</div>'
+            f'<div class="result">Tails count: {tails_count}</div>'
+            f'<div class="result">Heads percentage: {heads_percentage} %</div>'
+            f'<div class="result">Tails percentage: {tails_percentage} %</div>',
+            unsafe_allow_html=True
+        )
